5660f9e5bad2416ca1ee901ddcb239e45568e6c2
ArnoSchiller <arno-schiller@hotmail.de>
2023-06-27 12:55:05 +0200

    Optimized visualization, added color and hide functionality

--- js/main.js
+++ js/main.js
@@ -1,164 +1,263 @@
-import * as THREE from '../node_modules/three/build/three.module.js';
-import { TrackballControls } from '../node_modules/three/examples/jsm/controls/TrackballControls.js';
+import * as THREE from '../node_modules/three/build/three.module.js'
+import { TrackballControls } from '../node_modules/three/examples/jsm/controls/TrackballControls.js'
+
+const MAX_X = 8, MAX_Y = 10, MAX_Z = 12
+
+const BOTTOM_PADDING = 100
+
+const objColors = ["#537188", "#CBB279", "#E1D4BB"]
+
+const edit_texts = ["position x", "position y", "position z"] 
 
 class Position {
     constructor(x, y, z) {
-      this.x = x;
-      this.y = y;
-      this.z = z;
-    }
-  
-    describe() {
-      console.log(`Position: (${this.x}, ${this.y}, ${this.z})`);
+      this.x = x 
+      this.y = y 
+      this.z = z 
     }
 }
 
 class PackedObject3D {
-    constructor(width, height, depth, pos) {
-      this.width = width;
-      this.height = height;
-      this.depth = depth;
-      this.pos = pos;
-    }
+	constructor(name, width, height, depth) {
+		this.name = name 
+		this.width = width 
+		this.height = height 
+		this.depth = depth 
 
-	get position() {
-		return [this.pos.x + this.width / 2, this.pos.z + this.height / 2, this.pos.y + this.depth / 2];
+		this.pos = new Position(0, MAX_Y + 2, 0) 
+		this.visible = true
+		let col = objColors[Math.round(Math.random() * (objColors.length-1))]
+		this.colorString = LightenColor(col, Math.random() * 50)
+        this.color =  new THREE.Color(this.colorString)
+	}
+	
+	get position() {		
+		return [
+			this.pos.x + this.width / 2 - MAX_X / 2, 
+			this.pos.z + this.depth / 2 - MAX_Z / 2, 
+			this.pos.y + this.height / 2
+		] 
 	}
 
-    describe() {
-      console.log(`Object dimensions: ${this.width} x ${this.height} x ${this.depth}`);
-      this.pos.describe();
-    }
+	toString() {
+		return `${this.name} (${this.width} x ${this.height} x ${this.depth})` 
+	}
 }
-const edit_texts = ["position x", "position y", "position z"];
+
+
+const objects = [
+  	new PackedObject3D("Zaunelement", 1, 2, 3, null)
+]
 
 var objectsList = []
 
 var body = document.body,
-    html = document.documentElement;
-var height = Math.max( body.scrollHeight, body.offsetHeight, 
-                       html.clientHeight, html.scrollHeight, html.offsetHeight );
-const leftContainer = document.getElementById("editContainer");
-leftContainer.style.height = `${height-100}px`
-let editContainer = document.createElement("div");
-editContainer.style.height = "100%";
-editContainer.style.overflowY = "scroll";
+    html = document.documentElement 
+var height = Math.max( 
+  body.scrollHeight, body.offsetHeight, 
+  html.clientHeight, html.scrollHeight, html.offsetHeight 
+) 
 
-leftContainer.appendChild(editContainer);
 
-let newObjectBtn = document.createElement("button");
-newObjectBtn.innerHTML = "Add new";
-newObjectBtn.style.margin = "10px";
-newObjectBtn.style.padding = "10px";
-newObjectBtn.onclick = function () {
-  const pos = new Position(1,0,0);
-  const object = new PackedObject3D(2,3,2, pos);
-  objectsList.push(object)
-  createEditPanel(objectsList.length-1);
-  rendering()
-};
-leftContainer.appendChild(newObjectBtn);
+/* 
+#################################################################################
+#                              edit panel                                       #
+#################################################################################
+*/
 
-function createEditPanel(index) {
-  var wrapper = document.createElement('div');
-  wrapper.style.border = '1px solid';
-  wrapper.style.margin = '0.2em';
-  wrapper.style.padding = '0.2em';
+function createEditPanelForObject(container, index) {
+  var wrapper = document.createElement('div') 
+  wrapper.style.border = '1px solid' 
+  wrapper.style.borderRadius = '10px' 
+  wrapper.style.margin = '0.2em' 
+  wrapper.style.padding = '0.5em' 
+  
+  // header
+  const header = document.createElement('table') 
+  wrapper.appendChild(header)
+  const titleRow = header.insertRow() 
 
-  const header = document.createTextNode(`Item ${index+1}`);
-  wrapper.appendChild(header);
+  let bold = document.createElement('strong'),
+  textnode = document.createTextNode(objectsList[index].toString()) 
+  bold.appendChild(textnode); 
+  let tdTitle = titleRow.insertCell() 
+  tdTitle.style.width = "90%"
+  tdTitle.appendChild(bold) 
+  
+  let colorDiv = document.createElement('div')
+  colorDiv.style.width = "20px"
+  colorDiv.style.height = "20px"
+  colorDiv.style.backgroundColor = objectsList[index].colorString
+  let tdColor = titleRow.insertCell() 
+  tdColor.style.width = "10%"
+  tdColor.appendChild(colorDiv)  
 
-  const tbl = document.createElement('table');
+  // visibility checkbox
+  const visibility = document.createElement('table') 
+  wrapper.appendChild(visibility)
+  const visibilityRow = visibility.insertRow() 
+  let tdCheckbox = visibilityRow.insertCell() 
+  var checkbox = document.createElement('input')
+  checkbox.setAttribute("type", "checkbox") 
+  checkbox.innerHTML = 'ausblenden' 
+  checkbox.addEventListener('change', function() {
+    objectsList[index].visible = !this.checked
+    rendering() 
+  })
+  tdCheckbox.appendChild(checkbox)
+  let tdCheckboxLabel = visibilityRow.insertCell() 
+  var checkboxLabel = document.createTextNode("ausblenden")
+  tdCheckboxLabel.appendChild(checkboxLabel)
 
-  const tr = tbl.insertRow();
+
+  // edit table
+  const tbl = document.createElement('table') 
+  const tr = tbl.insertRow() 
   for (let j = 0; j < 3; j++) {
-    const td = tr.insertCell();
-    td.appendChild(document.createTextNode(edit_texts[j]));
+    const td = tr.insertCell() 
+    td.appendChild(document.createTextNode(edit_texts[j])) 
   }
   
-  const tr2 = tbl.insertRow();
+  const tr2 = tbl.insertRow() 
 
-  const tdX = tr2.insertCell();
-  var inputX = document.createElement("INPUT");
-  inputX.setAttribute("type", "number");
-  inputX.setAttribute("value", objectsList[index].pos.x);
-  tdX.appendChild(inputX);
+  const tdX = tr2.insertCell() 
+  var inputX = document.createElement("INPUT") 
+  inputX.setAttribute("type", "number") 
+  inputX.setAttribute("value", objectsList[index].pos.x) 
+  inputX.style.width = "70px" 
+  tdX.appendChild(inputX) 
   inputX.addEventListener("change", (event) => {
-    objectsList[index].pos.x = Number(inputX.value)
-    objectsList[index].describe()
-    rendering();
-  });
+	var value = Number(inputX.value)
+	let max = MAX_X - objectsList[index].width
+	if(value > max) { value = max; inputX.value = value }
+	if(value < 0) { value = 0; inputX.value = value }
 
-  const tdY = tr2.insertCell();
-  var inputY = document.createElement("INPUT");
-  inputY.setAttribute("type", "number");
-  inputY.setAttribute("value", objectsList[index].pos.y);
-  tdY.appendChild(inputY);
+    objectsList[index].pos.x = value
+    rendering() 
+  }) 
+
+  const tdY = tr2.insertCell() 
+  var inputY = document.createElement("INPUT") 
+  inputY.setAttribute("type", "number") 
+  inputY.setAttribute("value", objectsList[index].pos.y) 
+  inputY.style.width = "70px" 
+  tdY.appendChild(inputY) 
   inputY.addEventListener("change", (event) => {
-    objectsList[index].pos.y = Number(inputY.value)
-    objectsList[index].describe()
-    rendering();
-  });
+	var value = Number(inputY.value)
+	let max = MAX_Y - objectsList[index].height
+	if(value > max) { value = max; inputY.value = value }
+	if(value < 0) { value = 0; inputY.value = value }
+	
+    objectsList[index].pos.y = value
+    rendering() 
+  }) 
 
-  const tdZ = tr2.insertCell();
-  var inputZ = document.createElement("INPUT");
-  inputZ.setAttribute("type", "number");
-  inputZ.setAttribute("value", objectsList[index].pos.z);
-  tdZ.appendChild(inputZ);
+  const tdZ = tr2.insertCell() 
+  var inputZ = document.createElement("INPUT") 
+  inputZ.setAttribute("type", "number") 
+  inputZ.setAttribute("value", objectsList[index].pos.z) 
+  inputZ.style.width = "70px" 
+  tdZ.appendChild(inputZ) 
   inputZ.addEventListener("change", (event) => {
-    objectsList[index].pos.z = Number(inputZ.value)
-    objectsList[index].describe()
-    rendering();
-  });
+	var value = Number(inputZ.value)
+	let max = MAX_Z - objectsList[index].depth
+	if(value > max) { value = max; inputZ.value = value }
+	if(value < 0) { value = 0; inputZ.value = value }
 
-  wrapper.appendChild(tbl);
-  editContainer.appendChild(wrapper);
+    objectsList[index].pos.z = value
+    rendering() 
+  }) 
+
+  wrapper.appendChild(tbl) 
+  container.appendChild(wrapper) 
 }
 
-for (let i = 0; i < objectsList.length; i++) {
-  createEditPanel(i);
-}
+function createEditPanel() {
 
+  const leftContainer = document.getElementById("editContainer") 
+  leftContainer.style.height = `${height-100}px`
+
+  let editContainer = document.createElement("div") 
+  editContainer.style.height = "100%" 
+  editContainer.style.overflowY = "scroll" 
+  leftContainer.appendChild(editContainer) 
+  
+  
+  let newObjectSelect = document.createElement("select") 
+  newObjectSelect.style.margin = "10px" 
+  newObjectSelect.style.padding = "10px" 
+  for (let i = 0; i < objects.length; i++) {
+    const option = document.createElement("option") 
+    option.value = i 
+    option.text = objects[i].toString() 
+    newObjectSelect.add(option, null)
+  }
+  leftContainer.appendChild(newObjectSelect) 
+
+  // button to add new object
+  let newObjectBtn = document.createElement("button") 
+  newObjectBtn.innerHTML = "Add new" 
+  newObjectBtn.style.margin = "10px" 
+  newObjectBtn.style.padding = "10px" 
+  newObjectBtn.onclick = function () {
+	let obj = objects[newObjectSelect.value]
+	const object = new PackedObject3D(obj.name, obj.width, obj.height, obj.depth)
+    objectsList.push(object)
+    createEditPanelForObject(editContainer, objectsList.length-1) 
+    rendering()
+  } 
+  leftContainer.appendChild(newObjectBtn) 
+  
+  for (let i = 0; i < objectsList.length; i++) {
+    createEditPanelForObject(editContainer, i) 
+  }
+}
+createEditPanel()
+/* 
+#################################################################################
+#                               three.js                                        #
+#################################################################################
+*/
 // Scene
-const scene = new THREE.Scene();
-scene.rotation.z = 5 * Math.PI / 4;
-const rotAxis = new THREE.Vector3(1,0, 0);
-scene.rotateOnWorldAxis(rotAxis, 6.5 * Math.PI / 4);
+const scene = new THREE.Scene() 
+scene.rotation.z = 5 * Math.PI / 4 
+const rotAxis = new THREE.Vector3(1,0, 0) 
+scene.rotateOnWorldAxis(rotAxis, 6.5 * Math.PI / 4) 
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.6, 1200);
-camera.position.z = 20; // Set camera position
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / 2 / (window.innerHeight - BOTTOM_PADDING), 0.6, 1200) 
+camera.position.z = 30  // Set camera position
 // Renderer
-const renderer = new THREE.WebGLRenderer({antialias: true});
-renderer.setClearColor("#FFFFFF"); // Set background colour
-renderer.setSize(window.innerWidth/2, window.innerHeight / 2);
-document.getElementById("plotContainer").appendChild(renderer.domElement); // Add renderer to HTML as a canvas element
+const renderer = new THREE.WebGLRenderer({antialias: true}) 
+renderer.setClearColor("#FFFFFF")  // Set background colour
+renderer.setSize(window.innerWidth/2, window.innerHeight - BOTTOM_PADDING) 
+document.getElementById("plotContainer").appendChild(renderer.domElement)  // Add renderer to HTML as a canvas element
 
 // Add ambient light
-const light = new THREE.AmbientLight( 0xFFFFFF ); // soft white light
-scene.add( light );
+const light = new THREE.AmbientLight( 0xFFFFFF )  // soft white light
+scene.add( light ) 
 
 /*
 // Add axes helper
-const axesHelper = new THREE.AxesHelper( 10 );
-scene.add( axesHelper );
+const axesHelper = new THREE.AxesHelper( 10 ) 
+scene.add( axesHelper ) 
 
 // Add grid helper
-const size = 10;
-const divisions = 10;
-const gridHelper = new THREE.GridHelper( size, divisions );
-scene.add( gridHelper );
+const size = 10 
+const divisions = 10 
+const gridHelper = new THREE.GridHelper( size, divisions ) 
+scene.add( gridHelper ) 
 */ 
 //Trackball Controls for Camera 
-const controls = new TrackballControls(camera, renderer.domElement); 
-controls.rotateSpeed = 2;
-controls.dynamicDampingFactor = 0.15;
+const controls = new TrackballControls(camera, renderer.domElement)  
+controls.maxDistance = 40 
+controls.minDistance = 20 
 
 // Make Canvas Responsive
 window.addEventListener('resize', () => {
-    renderer.setSize(window.innerWidth / 2, window.innerHeight / 2); // Update size
-    camera.aspect = window.innerWidth / window.innerHeight; // Update aspect ratio
-    camera.updateProjectionMatrix(); // Apply changes
+	renderer.setSize(window.innerWidth/2, window.innerHeight - BOTTOM_PADDING)  
+    camera.aspect = window.innerWidth / 2 / (window.innerHeight - BOTTOM_PADDING)  // Update aspect ratio
+    camera.updateProjectionMatrix()  // Apply changes
 })
 
 // Load texture for palett
@@ -171,42 +270,83 @@ image.addEventListener('load', () =>
 image.src = '../static/textures/wood.jpg'
 
 // Create palett
-const palettDimension = [8, 12, 0.5]
-const palettGeometry = new THREE.BoxGeometry(palettDimension[0], palettDimension[1],palettDimension[2]); // Define geometry
-const palettMaterial = new THREE.MeshLambertMaterial({color: 0xe3e2ca, map: texture}); // Define material
-const palettMesh = new THREE.Mesh(palettGeometry, palettMaterial); // Build box
-palettMesh.position.set(palettDimension[0]/2, palettDimension[1]/2, -palettDimension[2]/2);
-scene.add(palettMesh); // Add box to canvas
+const palettGeometry = new THREE.BoxGeometry(MAX_X, MAX_Z, 0.5)  
+const palettMaterial = new THREE.MeshLambertMaterial({color: 0xe3e2ca, map: texture})  // Define material
+const palettMesh = new THREE.Mesh(palettGeometry, palettMaterial)  
+palettMesh.position.z = -0.25
+scene.add(palettMesh)  
 
-var objMaterial = new THREE.ShaderMaterial({
-  uniforms: {
-    thickness: {
-    	value: 1.5
-    }
-  },
-  vertexShader: vertexShader,
-  fragmentShader: fragmentShader
-});
+const points = [];
+points.push( new THREE.Vector3( -MAX_X/2, -MAX_Z/2, 0 ) );
+points.push( new THREE.Vector3( -MAX_X/2, -MAX_Z/2, MAX_Y ) );
+points.push( new THREE.Vector3( -MAX_X/2, MAX_Z/2, MAX_Y ) );
+points.push( new THREE.Vector3( -MAX_X/2, MAX_Z/2, 0 ) );
+
+points.push( new THREE.Vector3( -MAX_X/2, MAX_Z/2, MAX_Y ) );
+points.push( new THREE.Vector3( MAX_X/2, MAX_Z/2, MAX_Y ) );
+points.push( new THREE.Vector3( MAX_X/2, MAX_Z/2, 0 ) );
+
+points.push( new THREE.Vector3( MAX_X/2, MAX_Z/2, MAX_Y ) );
+points.push( new THREE.Vector3( MAX_X/2, -MAX_Z/2, MAX_Y ) );
+points.push( new THREE.Vector3( MAX_X/2, -MAX_Z/2, 0 ) );
+
+points.push( new THREE.Vector3( MAX_X/2, -MAX_Z/2, MAX_Y ) );
+points.push( new THREE.Vector3( -MAX_X/2, -MAX_Z/2, MAX_Y ) );
 
 
+
+const material = new THREE.LineBasicMaterial( { color: 0xaaaaaa } );
+const geometry = new THREE.BufferGeometry().setFromPoints( points );
+const line = new THREE.Line( geometry, material );
+scene.add( line );
+
+/* 
+#################################################################################
+#                            three.js rendering                                 #
+#################################################################################
+*/
 const rendering = function() {
   // Rerender every time the page refreshes (pause when on another tab)
-  requestAnimationFrame(rendering);
+  requestAnimationFrame(rendering) 
 	// Update trackball controls
-  controls.update();
+  controls.update() 
   
-  for (let i = 0; i < objectsList.length; i++) {
-    let obj = objectsList[i];
-    var object = scene.getObjectByName(`object_${i}`);
-    if (object == null) {
-      const objGeometry = new THREE.BoxGeometry(obj.width,obj.height, obj.depth); // Define geometry
-      const objMesh = new THREE.Mesh(objGeometry, objMaterial); // Build box
-      objMesh.position.set(obj.position[0], obj.position[1], obj.position[2]);
-      objMesh.name = `object_${i}`;
-      scene.add(objMesh); // Add box to canvas
-    }
-    object.position.set(obj.position[0], obj.position[1], obj.position[2]);
-  }
-	renderer.render(scene, camera);
+	for (let i = 0;  i < objectsList.length;  i++) {
+		let obj = objectsList[i] 
+		var object = scene.getObjectByName(`object_${i}`) 
+		if (object == null && obj.visible) {
+			const objGeometry = new THREE.BoxGeometry(obj.width, obj.depth, obj.height)  // Define geometry
+			var objMaterial = new THREE.MeshStandardMaterial({color: obj.color}); 
+			const objMesh = new THREE.Mesh(objGeometry, objMaterial)  // Build box
+			objMesh.position.set(
+				obj.position[0], 
+				obj.position[1], 
+				obj.position[2]
+				) 
+			objMesh.name = `object_${i}` 
+			scene.add(objMesh)  // Add box to canvas
+		} else {
+			if(obj.visible) {
+				object.position.set(
+					obj.position[0], 
+					obj.position[1], 
+					obj.position[2]
+				) 
+			} else {
+				scene.remove(object)
+			}
+		}
+	}
+	renderer.render(scene, camera) 
 }
-rendering();
+rendering() 
+
+
+function LightenColor(color, percent) {
+	var num = parseInt(color.replace("#",""),16),
+	amt = Math.round(percent),
+	R = (num >> 16) + amt,
+	B = (num >> 8 & 0x00FF) + amt,
+	G = (num & 0x0000FF) + amt;
+	return "#" + (0x1000000 + (R<255?R<1?0:R:255)*0x10000 + (B<255?B<1?0:B:255)*0x100 + (G<255?G<1?0:G:255)).toString(16).slice(1);
+};
